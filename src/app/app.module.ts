c87c3c13dd2f1ee7d2afacffa073fc27df1fb87c
Kaabvoo <jgcavazos96@hotmail.com>
2018-05-31 20:43:33 -0500

    Create all other componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { TorsoComponent } from './torso/torso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    TorsoComponent
   ],
   imports: [
     BrowserModule
